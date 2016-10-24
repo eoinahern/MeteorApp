433b3a9a406e10e24a4a771d89b20f6b9d9a56f6
eoinahern <eoin.ahern@mycit.ie>
2016-10-24 22:53:10 +0100

    added annotations

--- app/src/main/java/com/example/eoin_a/meteorapp/Data/entity/GeoLocation.java
+++ app/src/main/java/com/example/eoin_a/meteorapp/Data/entity/GeoLocation.java
@@ -1,11 +1,14 @@
 package com.example.eoin_a.meteorapp.Data.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by eoin_a on 24/10/2016.
  */
 
 public class GeoLocation {
 
+    @SerializedName("coordinates")
     private  float[] coordinates;
 
     public GeoLocation()
